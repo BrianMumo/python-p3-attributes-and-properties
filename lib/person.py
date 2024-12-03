0c3d40d6257f7f6f0b9b6396a7824f4eefc4ce56
Brian <mumobrian2746@gmail.com>
2024-12-03 12:53:37 +0300

    Add validation properties for Dog and Person classes

--- lib/person.py
+++ lib/person.py
@@ -16,4 +16,35 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    approved_jobs = [
+        "Admin", "Customer Service", "Human Resources", "ITC", 
+        "Production", "Legal", "Finance", "Sales", 
+        "General Management", "Research & Development", 
+        "Marketing", "Purchasing"
+    ]
+
+    def __init__(self, name="Unknown", job="Admin"):
+        self.name = name
+        self.job = job
+
+    @property
+    def name(self):
+        return self._name
+
+    @name.setter
+    def name(self, name):
+        if isinstance(name, str) and 1 <= len(name) <= 25:
+            self._name = name.title()  # Convert to title case
+        else:
+            print("Name must be string between 1 and 25 characters.")
+
+    @property
+    def job(self):
+        return self._job
+
+    @job.setter
+    def job(self, job):
+        if job in Person.approved_jobs:
+            self._job = job
+        else:
+            print("Job must be in list of approved jobs.")
